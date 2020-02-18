b3eb8a330ffd5585b4b318ac9b19fbc0c4d3a200
syun413 <56644064+syun413@users.noreply.github.com>
2020-02-18 21:32:00 +0800

    2020/2/18 作業3.6

---
+++ 第3章/作業3.6.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+double a,b,c,d;
+scanf("%lf %lf %lf %lf",&a,&b,&c,&d);
+	if((a/b) == (c/d))
+		printf("1");
+	else
+		printf("0"); 
+	return 0;
+} 
